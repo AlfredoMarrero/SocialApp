53e523b8550b03f8e3c2432b90b5c3b8ba850cf8
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-03-08 16:50:39 -0500

    Add custom classes to sign in UI.

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Alfredo M. on 3/8/17.
+//  Copyright © 2017 Alfredo M. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255
