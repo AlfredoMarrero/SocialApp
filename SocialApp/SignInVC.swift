e997e24b7d6f92505ca8e84ef328a2f5e9851876
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-03-09 12:18:45 -0500

    Add facebook authentication.

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookButtonTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { result, error in
+                      print(result.debugDescription)
+            if error != nil {
+                print("Error: Unable to authenticate with Facebook.")
+            } else if result?.isCancelled == true {
+                print ("User cancelled Facebook authentication.")
+            } else {
+                print ("Successfully authentucated with Facebook.")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
     }
 
-
+    func firebaseAuthenticate(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: {user, error in
+            if error != nil {
+                print("Error: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print ("Successfully authenticated with Firebase.")
+            }
+        })
+    }
 }
 
